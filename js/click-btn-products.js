4c623ae03c20dea561eaea97e1bec78d8e55aa4e
Tonya-Shyva <toshash69@gmail.com>
2022-08-29 21:02:14 +0300

    e.target??? how to click on btn(include svg and use) change arrow`s direction

--- js/click-btn-products.js
+++ js/click-btn-products.js
@@ -1,21 +1,60 @@
 const productsListRef = document.querySelector('.products__list');
-const menuBtnRef = document.querySelectorAll('.products__list button');
-const mobileMenuRef = document.querySelector('.products__card div');
+const menuProductBtnRef = document.querySelectorAll('.products__list button');
 
-function onBtnClick() {
-  const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-  menuBtnRef.classList.toggle('is-open');
-  menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-  mobileMenuRef.classList.toggle('is-open');
+function onBtnClick(e) {
+  for (const btn of menuProductBtnRef) {
+    if (e.target === btn) {
+      btn.classList.toggle('is-open');
+    }
+  }
 }
 
-productsListRef.addEventListener('click', function (event) {
-  if (event.target(menuBtnRef)) {
-    onBtnClick();
-  }
-});
+productsListRef.addEventListener('click', onBtnClick);
+
+// (() => {
+//   const menuBtnRef = document.querySelector('.products__btn1');
+//   const mobileMenuRef = document.querySelector('.products__article1');
+
+//   menuBtnRef.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+//     menuBtnRef.classList.toggle('is-open');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+// })();
+
+// (() => {
+//   const menuBtnRef = document.querySelector('.products__btn2');
+//   const mobileMenuRef = document.querySelector('.products__article2');
+
+//   menuBtnRef.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+//     menuBtnRef.classList.toggle('is-open');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+// })();
+
+// (() => {
+//   const menuBtnRef = document.querySelector('.products__btn3');
+//   const mobileMenuRef = document.querySelector('.products__article3');
+
+//   menuBtnRef.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+//     menuBtnRef.classList.toggle('is-open');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+// })();
 
 $('.products__btn1').click(function () {
   $('.products__article1').fadeToggle(500);
