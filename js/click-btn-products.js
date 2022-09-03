390905ff64c427f27a29cf08a08583690e51164d
Tonya-Shyva <toshash69@gmail.com>
2022-09-03 19:01:23 +0300

    optimized code in click-btn-products.js

--- js/click-btn-products.js
+++ js/click-btn-products.js
@@ -3,58 +3,17 @@ const menuProductBtnRef = document.querySelectorAll('.products__list button');
 
 function onBtnClick(e) {
   for (const btn of menuProductBtnRef) {
-    if (e.target === btn) {
+    if (
+      e.target === btn ||
+      btn.firstElementChild === e.target ||
+      btn.firstElementChild === e.path[1]
+    ) {
       btn.classList.toggle('is-open');
     }
   }
 }
 
 productsListRef.addEventListener('click', onBtnClick);
-
-// (() => {
-//   const menuBtnRef = document.querySelector('.products__btn1');
-//   const mobileMenuRef = document.querySelector('.products__article1');
-
-//   menuBtnRef.addEventListener('click', () => {
-//     const expanded =
-//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-//     menuBtnRef.classList.toggle('is-open');
-//     menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-//     mobileMenuRef.classList.toggle('is-open');
-//   });
-// })();
-
-// (() => {
-//   const menuBtnRef = document.querySelector('.products__btn2');
-//   const mobileMenuRef = document.querySelector('.products__article2');
-
-//   menuBtnRef.addEventListener('click', () => {
-//     const expanded =
-//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-//     menuBtnRef.classList.toggle('is-open');
-//     menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-//     mobileMenuRef.classList.toggle('is-open');
-//   });
-// })();
-
-// (() => {
-//   const menuBtnRef = document.querySelector('.products__btn3');
-//   const mobileMenuRef = document.querySelector('.products__article3');
-
-//   menuBtnRef.addEventListener('click', () => {
-//     const expanded =
-//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-//     menuBtnRef.classList.toggle('is-open');
-//     menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-//     mobileMenuRef.classList.toggle('is-open');
-//   });
-// })();
 
 $('.products__btn1').click(function () {
   $('.products__article1').fadeToggle(500);
