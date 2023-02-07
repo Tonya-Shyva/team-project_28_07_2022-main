86548b0af342696b5d10ebdd845950fd0c76431a
Tonya-Shyva <toshash69@gmail.com>
2023-02-07 09:42:09 +0200

    Update click-btn-products.js

--- js/click-btn-products.js
+++ js/click-btn-products.js
@@ -1,15 +1,7 @@
-const productsListRef = document.querySelector('.products__list');
 const menuProductBtnRef = document.querySelectorAll('.products__list button');
 
-productsListRef.addEventListener('click', onBtnClick);
-
-function onBtnClick(e) {
-  menuProductBtnRef.forEach(btn => {
-    if (
-      e.target === btn ||
-      e.target === btn.firstElementChild ||
-      e.path[1] === btn.firstElementChild
-    )
-      btn.classList.toggle('is-open');
-  });
-}
+menuProductBtnRef.forEach(btn =>
+  btn.addEventListener('click', e => {
+    if (e.currentTarget === btn) btn.classList.toggle('is-open');
+  })
+);
