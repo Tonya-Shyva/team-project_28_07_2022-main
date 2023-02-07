b1b582fc12c3d829b601fa0bfc16cfd336f56474
Tonya-Shyva <toshash69@gmail.com>
2023-02-07 13:15:49 +0200

    corrected js(removed deprecated e.path)
    corrected animation

--- js/click-btn-products.js
+++ js/click-btn-products.js
@@ -2,6 +2,8 @@ const menuProductBtnRef = document.querySelectorAll('.products__list button');
 
 menuProductBtnRef.forEach(btn =>
   btn.addEventListener('click', e => {
-    if (e.currentTarget === btn) btn.classList.toggle('is-open');
+    if (e.currentTarget === btn) {
+      btn.classList.toggle('is-open');
+    }
   })
 );
